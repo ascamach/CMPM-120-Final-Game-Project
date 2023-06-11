e07c70dc516af7d4e5d509ec9a6fb880d5700064
Addison Camacho <ascamach@ucsc.edu>
2023-06-10 17:34:46 -0700

    Adding json parsing, working on story flow

--- test.js
+++ test.js
@@ -12,11 +12,13 @@ class Test extends Phaser.Scene {
         this.load.path = "./assets/";
         this.load.image("pizza", "test_pizza.jpg");
         this.load.image("shinji", "test_" + "shinji" + ".png");
+        this.load.json("dialogue", "test_dialogue.json");
     }
 
     create() {
         
         this.background = this.add.image(0, 0, "pizza").setOrigin(0, 0);
+        let dialogue_text = this.cache.json.get("dialogue").dialogue;
 
         this.background.displayWidth = this.game.canvas.width;
         this.background.displayHeight = this.game.canvas.height;
@@ -51,6 +53,28 @@ class Test extends Phaser.Scene {
             tr: 32,
             bl: 16,
             br: 0
+        });
+
+        let startNode = 'test';
+        let text_index = 0;
+
+        let node = dialogue_text[startNode];
+
+        let dia_text = this.add.text(100, 600, node.text[text_index]);
+
+        this.input.on('pointerdown', () => {
+            this.add.text(100, 500, node.text.length);
+            if (text_index == node.text.length) {
+                dia_text.destroy();
+                startNode = node.next;
+                node = dialogue_text[startNode];
+                text_index = 0;
+                dia_text = this.add.text(100, 600, node.text[text_index]);
+            } else {
+                dia_text.destroy();
+                text_index++;
+            }
+            dia_text = this.add.text(100, 600, node.text[text_index]);
         });
 
         this.onEnter();
